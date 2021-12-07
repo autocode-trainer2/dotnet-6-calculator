331d181b31200baa43d554833c240017aa5ae408
Kirill Peshin <kirill_peshin@epam.com>
2021-12-07 14:29:23 +0300

    01

--- FancyCalc/FancyCalc.cs
+++ FancyCalc/FancyCalc.cs
@@ -4,12 +4,14 @@
     {
         public double Add(int a, int b)
         {
+            // commented out code
             return a + b;
         }
 
 
         public double Subtract(int a, int b)
         {
+            int c = 10; //useless variable and commented out code
             return a - b;
         }
 
